aa9effb581f5e55c742a569373dad1a84f93f93a
William Silberstein <silberstein75@gmail.com>
2023-01-30 16:04:07 -0600

    Started on About Section

--- williamsilberste.in/src/App.js
+++ williamsilberste.in/src/App.js
@@ -7,6 +7,7 @@ import MainComponent from './components/MainContent';
 // import WaveAnimation from './components/WaveAnimation';
 
 const nav_pages = ['Home', 'About', 'Experience', 'Education', 'Projects', 'Contact'];
+const about_me_text = 'I love to build things. Whether its building a lawn chair or creating a multi-platform application, I seek out the sense of accomplishment for creating useful products in everything I do. In addition, I have a passion for learning and self growth. Since my first introduction to coding as an 11 year old, I have dedicated myself to learning the world of programming. I am most experienced with the OOP languages such as Java and C++, but I also have tons of experience with languages such as Javascript and C. In addition to my interests as a programmer, I am interested in sports and music. I am a big fan of the New York Rangers and am a firm believer that next year will be their year.'
 
 function App() {
 
@@ -24,7 +25,7 @@ function App() {
       <NavBarSide nav_pages={nav_pages} toggle={!inViewport} />
       {/* <WaveAnimation /> */}
       <div ref={ref} style={{position: 'absolute', zIndex: -10, height: '50vh', borderBottom: '1px solid black'}}>Test</div>
-      <MainComponent toggle={!inViewport} />
+      <MainComponent toggle={!inViewport} about_me_text={about_me_text}/>
     </>
   );
 }
--- williamsilberste.in/src/components/MainContent.jsx
+++ williamsilberste.in/src/components/MainContent.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import {
     Box,
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
     Collapse,
     Container,
+    Divider,
+    Heading,
     Image,
+
     Stack,
     Text,
     useDisclosure,
@@ -15,6 +22,9 @@ import {
 function MainComponent(props) {
 
     const toggle = props.toggle
+    const about_me_text = props.about_me_text
+
+    console.log(about_me_text)
 
     const [show, setShow] = useState(props.toggle)
     const [isMobile] = useMediaQuery("(min-width: 768px)")
@@ -24,8 +34,6 @@ function MainComponent(props) {
 
   return (
     <>
-        
-        
         <motion.div
         {...getDisclosureProps()}
         hidden={false}
@@ -43,36 +51,54 @@ function MainComponent(props) {
           zIndex: '-1'
         }}
       >
-        <InsideContent />
+        <InsideContent about_me_text={about_me_text}/>
       </motion.div>
     </>
     
   );
 }
 
-function AboutSection() {
+function AboutSection({about_me_text}) {
+
+  console.log()
+
   return (
     <>
-    <Stack direction={['column', 'row']}>
-      <Image
-        // borderRadius={'full'}
-        height={'md'}
-        // boxSize={'150px'}
-        src='/images/headshot.JPG'
-        alt='William Silberstein'
-      />
-      <Box overflowWrap={'normal'}>
-        <Text>
-        laksjdflaj sdl;f jas;ldfj l;kas djflk;asj dfl;kaj sd;lkf jas;lkgaksdjlfh vljkasjllkv asld kajchv ashdas dfl as;dkf ;askd flas dklas vlkslfkj sadlkfjal;svj lkasjdf lsdaj fl
-        </Text>
-      </Box>
+    <Box 
+      borderRadius={'lg'} 
+      border={'1px'}
+      borderColor={'gray.700'} 
+      margin={'3'}
+    >
+      <Stack 
+        direction={['column', 'row']} 
+        w={'full'} 
+        p={'4'} 
+        maxW={'full'}
+        spacing={'10'}
+      >
+        <Image
+          borderRadius={'full'}
+          height={'md'}
+          src='/images/headshot.JPG'
+          alt='William Silberstein'
+        />
+    
+        <Box w={'full'} whiteSpace={'normal'}>
+          <Heading pb={'10'} whiteSpace={'normal'}>About Me</Heading>
+          <Text fontSize={'xl'}>
+          {about_me_text['about_me_text']}
+          </Text>
+        </Box>
+        
 
-    </Stack>
+      </Stack>
+    </Box>
     </>
   )
 }
 
-function InsideContent() {
+function InsideContent(about_me_text) {
     return (
       <>
         <Box 
@@ -85,13 +111,8 @@ function InsideContent() {
           zIndex={'-1'}
         >
         </Box>
-        <AboutSection />
-        <Box h={'100vh'} zIndex={'-1'} backgroundColor={'green.400'}>
-          <Text>
-            TESTING
-          </Text>
-          Test3
-        </Box>
+        <AboutSection about_me_text={about_me_text}/>
+        <Box h={'200vh'}></Box>
       </>
       
     )
